d026ceabfef9cd16aeaae638b565430203c74235
alnex <54508612+alnexer@users.noreply.github.com>
2020-09-18 22:03:57 +0300

    version 1.0.3

--- GroupClass.js
+++ GroupClass.js
@@ -4,9 +4,9 @@ function unique(arr) {
     let result = []
 
     for (let str of arr) {
-    if (!result.includes(str)) {
-        result.push(str)
-    }
+        if (!result.includes(str)) {
+            result.push(str)
+        }
     }
 
     return result
@@ -19,39 +19,31 @@ class Group {
         this.setToAllGroups(this.name, array)
     }
 
-    setToAllGroups(name, array) { 
+    setToAllGroups(name, array) {
         array.push(name)
-
         array = unique(array)
-
         console.log(array)
 
         localStorage.setItem('All Groups', JSON.stringify(array))
-    }    
+    }
 
     getFromLS(name) {
         let nameR = name || this.name
+
         return JSON.parse(localStorage.getItem(nameR))
     }
 
     setToLS(inpVal) {
         this.arr.push(inpVal)
 
-        console.log(this.arr)
-
         localStorage.setItem(this.name, JSON.stringify(this.arr))
     }
 
-    
     doNote(keyObj, toDoPlace) {
         let key = keyObj.parentNode.querySelector('h3').getAttribute('data-id')
 
         toDoPlace.arr.push(this.arr[key])
-
-        console.log(key)
-
         this.arr.splice(key, 1)
-
         localStorage.setItem(this.name, JSON.stringify(this.arr))
         localStorage.setItem(toDoPlace.name, JSON.stringify(toDoPlace.arr))
     }
@@ -60,31 +52,28 @@ class Group {
         let needableArray = this.arr
 
         needableArray.reverse().splice(key, 1)
-
         needableArray.reverse()
-
         localStorage.setItem(this.name, JSON.stringify(needableArray))
     }
 
     innerAll(docObj, doIt, rev, doItRes) {
         let reverS = rev || true
         let doItResult = doItRes || ''
-        if(doIt){  
+
+        if (doIt) {
             doItResult = `<button class="doNote" data-r="${this.name}">✔</button>
             <button class="delNote" data-r="${this.name}">-</button>`
-        }else{
+        } else {
             doItResult = `<button class="delNote" data-r="${this.name}">-</button>`
         }
-        
-        let needableArray = this.arr.slice()       
 
-        if(reverS){
+        let needableArray = this.arr.slice()
+
+        if (reverS) {
             needableArray.reverse()
         }
 
-        console.log(needableArray, this.arr)
-
-        for(let key in needableArray){
+        for (let key in needableArray) {
             $(docObj).append(`
             <div class="note">
                 <h3 data-id='${key}'>${needableArray[key]}</h3>
--- newScript.js
+++ newScript.js
@@ -1,9 +1,10 @@
-
 const addNoteBtn = $('.addNote')
 const extraNoteField = $('.extraNoteField')
 const adderForm = $('form.adderForm')
 const adderGroupForm = $('form.adderGroupForm')
 const addGroup = $('.addGroup')
+
+const lS = localStorage
 
 addNoteBtn.on('click', OpenExtraFiled)
 addGroup.on('click', OpenGroupCreationField)
@@ -11,9 +12,6 @@ addGroup.on('click', OpenGroupCreationField)
 
 const allNotes = new Group('Все записи')
 const doneNotes = new Group('Сделанные')
-
-// выбранная группа // определить роутером
-
 
 currenGroup.innerAll('.allNotes', true, true)
 
@@ -23,7 +21,7 @@ function OpenExtraFiled() {
     $('.nameNote').focus()
 }
 
-function OpenGroupCreationField(){
+function OpenGroupCreationField() {
     $('.groupCreationField').toggle(200, 'swing')
     $('.nameGroup').focus()
 }
@@ -34,44 +32,41 @@ adderForm.on('submit', (e) => {
     let inpVal = $('input.nameNote').val()
 
     currenGroup.setToLS(inpVal)
-
     window.location.reload()
 })
 
 adderGroupForm.on('submit', (e) => {
     e.preventDefault()
-    
+
     let inpVal = $('input.nameGroup').val()
-
     let newGroup = new Group(inpVal)
-
-    console.log(array)
 
     window.location.reload()
 })
 
 $('.doNote').on('click', (e) => {
-    currenGroup.doNote(e.currentTarget, doneNotes)
+    const eTarget = e.currentTarget
+    currenGroup.doNote(eTarget, doneNotes)
     location.reload()
-}) 
+})
 
 $('.delNote').on('click', (e) => {
-    let key = e.currentTarget.parentNode.querySelector('h3').getAttribute('data-id')
+    const eTarget = e.currentTarget
+    let key = eTarget.parentNode.querySelector('h3').getAttribute('data-id')
 
     currenGroup.delFromLS(key)
-
     window.location.reload()
-}) 
+})
 
-// все массивы / todo: подключить роутинг страниц, на основах всех групп
-let newOldArr = JSON.parse(localStorage.getItem('All Groups'))
+let newOldArr = JSON.parse(lS.getItem('All Groups'))
 
 $('h2.current').html(`${currenGroup.name} <span>${currenGroup.arr.length}</span>`)
 
-for(key in newOldArr){
-    console.log(key, newOldArr[key])
-    var thisLength = JSON.parse(localStorage.getItem(newOldArr[key])) || []
-    if(newOldArr[key] != currenGroup.name){
-    $('.allGrInBlock').append(`<a href="/?g=${newOldArr[key]}"><h2 class='${newOldArr[key]}'>${newOldArr[key]} <span>${thisLength.length}</span></h2></a>`)
+
+for (key in newOldArr) {
+    let thisLength = JSON.parse(lS.getItem(newOldArr[key])) || []
+
+    if (newOldArr[key] != currenGroup.name) {
+        $('.allGrInBlock').append(`<a href="/?g=${newOldArr[key]}"><h2 class='${newOldArr[key]}'>${newOldArr[key]} <span>${thisLength.length}</span></h2></a>`)
     }
 }
